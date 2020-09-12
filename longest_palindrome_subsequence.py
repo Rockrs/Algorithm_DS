33e99e1f87b8905b0f469168104c838d8114ca85
Abhishek Sharma <shrmabhishek2012@gmail.com>
2020-09-12 17:40:43 +0530

    Add files via upload

---
+++ longest_palindrome_subsequence.py
@@ -0,0 +1,25 @@
+dp = [[0]*13 for _ in range(13)]
+
+def l_palindrome(s,i,j):
+    if i==j:
+        return 1
+    if i>j:
+        return 0
+    
+    if dp[i][j] is not 0:
+        return dp[i][j]
+    
+    
+    if s[i]==s[j]:
+        dp[i][j]= 2+l_palindrome(s,i+1,j-1)
+        return dp[i][j]
+    else:
+		dp[i][j] = max(l_palindrome(s,i,j-1),l_palindrome(s,i+1,j))
+        return dp[i][j]
+        
+    return dp[len(s)-1][len(s)-1]
+
+
+s = "geeksforgeeks"
+l = len(s)
+print(l_palindrome(s,0,l-1))
