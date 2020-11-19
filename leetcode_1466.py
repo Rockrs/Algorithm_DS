ce4273a1ce47393a3751e590a957dd76af32e89a
Abhi <shrmabhishek2012@gmail.com>
2020-11-19 10:02:23 +0530

    Some Changes to Leetcode_1466

--- leetcode_1466.py
+++ leetcode_1466.py
@@ -3,17 +3,20 @@ class Solution:
     def minReorder(self, n: int, edges: List[List[int]]) -> int:
 
         output = 0
-        edge_set = set()
-        visited = set()
-        adj = defaultdict(list)
-        adj_stk = [0]
-        visited.add(0)
+        edge_set = set()        # Set for storing edges
+        visited = set()         # Set for tracking visited edges while doing DFS
+        adj = defaultdict(list) # Dictioanry to store adjacent/neighbours
+        adj_stk = [0]           # stack to be used for DFS
+        visited.add(0)          # Initialize visited set with 0
 
+
+        #Procedure to store edges as a set and make dictionary for each node with value as neighbours
         for edge in edges:
             edge_set.add(tuple(edge))
             adj[edge[0]].append(edge[1])
             adj[edge[1]].append(edge[0])
 
+        # Simple DFS to check for each node with its neighbour wheather they need to be modofied or not
         while adj_stk!=[]:
             p =adj_stk.pop()
             for neigh in adj[p]:
